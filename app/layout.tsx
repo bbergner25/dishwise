fd91cc44a79ceef9aae1896f0645bd7e51dc8265
bbergner25 <will.bergner@gmail.com>
2026-04-26 13:43:18 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,12 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head><link rel="apple-touch-icon" href="/icon-192.png"/></head>
+      <head>
+        <link rel="apple-touch-icon" href="/icon-192.png"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9,700;1,9,700&family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+      </head>
       <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
